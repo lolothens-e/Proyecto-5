8228d3e75f06a454de09998f6ab39739c949482d
lolothens-e <annacast@espol.edu.ec>
2025-07-29 21:44:10 -0500

    Guia21

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "homepage",
+    "landing_api", 
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CREDENTIALS_PATH, {
+   'databaseURL': 'https://jmstore-c1903-default-rtdb.firebaseio.com/'
+})
--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include("homepage.urls")),
+    path('landing/api/', include("landing_api.urls")),
 ]
